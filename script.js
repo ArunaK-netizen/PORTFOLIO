b0c925326a88ba60d48cfa50227c8a41ad2b8aa1
ArunaK-netizen <79011165+ArunaK-netizen@users.noreply.github.com>
2024-01-29 05:56:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const profession = document.getElementById("profession");
+    const newprofession = document.getElementById("new-profession"); 
+    const jobs = ["  Developer"];
+    const writing_speed = 250;
+    let jobIndex = 0;
+    let charIndex = 0;
+    
+    function writing() {
+        if (jobIndex < jobs.length) {
+            if (charIndex < jobs[jobIndex].length) {
+                profession.innerHTML += jobs[jobIndex][charIndex];
+                
+                charIndex++;
+                setTimeout(writing, writing_speed);
+            } else {
+                jobIndex++;
+                charIndex = 0;
+                setTimeout(writing, writing_speed);
+            }
+        }
+
+        else{
+            
+            profession.style.textDecoration = "line-through";
+            newprofession.style.opacity = "1";
+        }
+        
+    }
+    
+    writing();
+
+    
+    
+    
+});
